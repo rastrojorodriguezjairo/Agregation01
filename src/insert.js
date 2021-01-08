c8647d210f697e0c17df2d02fd110a8947bd9ccf
Your Name <you@example.com>
2021-01-08 09:09:45 +0100

    Aggregations

---
+++ src/insert.js
@@ -0,0 +1,14 @@
+db.empleados.insertMany([
+{ nombre: "Jairo", apellido: "Rastrojo", sueldo:1200, probincia: "Sevilla"},
+{ nombre: "Ahri", apellido: "Ninetale", sueldo:950, probincia: "Sevilla"},
+{ nombre: "Tokisaki", apellido: "Kurumi", sueldo:1500, probincia: "Sevilla"},
+{ nombre: "Jose", apellido: "Palacios", sueldo:800, probincia: "Sevilla"},
+{ nombre: "Francisco", apellido: "Laguna", sueldo:1100, probincia: "Sevilla"},
+{ nombre: "Laura", apellido: "Mejia", sueldo:950, probincia: "Huelva"},
+{ nombre: "Marcos", apellido: "Suarez", sueldo:750, probincia: "Huelva"},
+{ nombre: "Ruben", apellido: "Lopez", sueldo:900, probincia: "Huelva"},
+{ nombre: "Daniel", apellido: "Marquez", sueldo:1250, probincia: "Cordoba"},
+{ nombre: "Miguel", apellido: "Sanchez", sueldo:950, probincia: "Cordoba"},
+{ nombre: "Ricardo", apellido: "Entrena", sueldo:600, probincia: "Cadiz"},
+{ nombre: "Ruben", apellido: "Medina", sueldo:600, probincia: "Cadiz"},
+ ]);
